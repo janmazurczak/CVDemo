2ac4dc3486238b000fb47b5f364cc2b4aed629b4
Jan Mazurczak <janmazurczak@icloud.com>
2020-06-02 20:49:26 +0200

    Download and cache.

--- CVDemo/Logic/DefaultExplorationFlow.swift
+++ CVDemo/Logic/DefaultExplorationFlow.swift
@@ -10,6 +10,35 @@ import Foundation
 
 class DefaultExplorationFlow: ExplorationFlow {
     func requestData(for presenter: Presenter) {
-        
+        let cachedCV =
+            CV(contentsOf: .cvCache) ?? // Check what's stored already
+                CV(version: -1, root: .init(title: "Loading...", items: [])) // Create a placeholder if nothing is stored
+        presenter.present(cachedCV)
+        Configuration.current.communicator.fetch(CV.self, at: ["getCV"]) { [weak presenter] in
+            guard let cv = $0 else { return }
+            try? JSONEncoder().encode(cv).write(to: .cvCache)   // Store what's downloaded
+            guard cv.version > cachedCV.version else { return } // Don't refresh if there is no newer version
+            presenter?.present(cv)
+        }
     }
 }
+
+extension CV {
+    init?(contentsOf url: URL) {
+        guard
+            let data = try? Data(contentsOf: url),
+            let cv = try? JSONDecoder().decode(CV.self, from: data)
+            else { return nil }
+        self = cv
+    }
+}
+
+extension URL {
+    static let cvCache: URL = cache.appendingPathComponent("cv.json", isDirectory: false)
+    static let cache: URL = {
+        let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("CV3DCache", isDirectory: true)
+        try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        return url
+    }()
+}
+
--- CVDemo/Logic/StandardBackendCommunicator.swift
+++ CVDemo/Logic/StandardBackendCommunicator.swift
@@ -9,10 +9,23 @@
 import Foundation
 
 class StandardBackendCommunicator: BackendCommunicator {
+    let endpoint: URL
+    private let queue: OperationQueue
     init(at endpoint: URL) {
-        
+        self.endpoint = endpoint
+        self.queue = OperationQueue()
     }
     func fetch<Result>(_ resultType: Result.Type, at path: [String], completion: @escaping (Result?) -> Void) where Result : Decodable {
-        
+        let url = path.reduce(endpoint) { $0.appendingPathComponent($1) }
+        queue.addOperation {
+            var result: Result?
+            defer {
+                OperationQueue.main.addOperation {
+                    completion(result)
+                }
+            }
+            guard let data = try? Data(contentsOf: url) else { return }
+            result = try? JSONDecoder().decode(Result.self, from: data)
+        }
     }
 }
